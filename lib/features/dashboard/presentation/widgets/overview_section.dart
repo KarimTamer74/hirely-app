10d012a670be2a27ed4780bcf13ce5e190e489b7
KarimTamer74 <kemotamer448@gmail.com>
2025-07-06 01:35:13 +0300

    overview section in dashboard screen done

--- lib/features/dashboard/presentation/widgets/overview_section.dart
+++ lib/features/dashboard/presentation/widgets/overview_section.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hirely/core/theme/app_colors.dart';
-import 'package:hirely/core/utils/app_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hirely/core/utils/app_strings.dart';
 import 'package:hirely/core/utils/app_styles.dart';
-import 'package:hirely/core/utils/font_manager.dart';
-import 'package:hirely/core/utils/style_manager.dart';
+import 'package:hirely/features/dashboard/presentation/widgets/application_status_card_widget.dart';
+import 'package:hirely/features/dashboard/presentation/widgets/total_applications_card_widget.dart';
 
 class OverviewSection extends StatelessWidget {
   const OverviewSection({super.key});
@@ -19,46 +18,31 @@ class OverviewSection extends StatelessWidget {
           AppStrings.overview,
           style: AppStyles.textStyleBold22,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Card(
-              color: AppColor.white,
-              child: Column(
+        LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth < 200) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    AppStrings.totalApplications,
-                    style: AppStyles.textStyle16,
-                  ),
-                  Text('20', style: getBoldStyle(fontSize: FontSize.s24))
+                  TotalApplicationsCardWidget(),
+                  12.verticalSpace,
+                  ApplicationStatusCardWidget(),
                 ],
-              ),
-            ),
-            Card(
-              color: AppColor.white,
-              child: Column(
-                children: [
-                  Text(
-                    AppStrings.status,
-                    style: AppStyles.textStyle16,
-                  ),
-                  ListTile(
-                    leading: Container(
-                      width: 15,
-                      height: 15,
-                      decoration: BoxDecoration(
-                          color: AppColors.redShade900,
-                          borderRadius: BorderRadius.circular(5)),
-                    ),
-                    title:
-                        Text(AppStrings.pending, style: AppStyles.textStyle16),
-                    trailing:
-                        Text('10', style: getBoldStyle(fontSize: FontSize.s24)),
-                  )
-                ],
-              ),
-            ),
-          ],
+              );
+            } else {
+              return IntrinsicHeight(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(child: TotalApplicationsCardWidget()),
+                    12.horizontalSpace,
+                    Expanded(child: ApplicationStatusCardWidget()),
+                  ],
+                ),
+              );
+            }
+          },
         )
       ],
     );
