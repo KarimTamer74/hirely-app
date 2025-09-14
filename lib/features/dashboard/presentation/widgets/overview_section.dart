90f8bd931ed235da0341420b5f221e0d858361ec
KarimTamer74 <kemotamer448@gmail.com>
2025-09-14 14:16:56 +0300

    overview section done

--- lib/features/dashboard/presentation/widgets/overview_section.dart
+++ lib/features/dashboard/presentation/widgets/overview_section.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hirely/core/network/job_model.dart';
 import 'package:hirely/core/utils/app_strings.dart';
 import 'package:hirely/core/utils/app_styles.dart';
 import 'package:hirely/features/dashboard/presentation/widgets/application_status_card_widget.dart';
+import 'package:hirely/features/dashboard/presentation/widgets/application_type_card_widget.dart';
 import 'package:hirely/features/dashboard/presentation/widgets/total_applications_card_widget.dart';
 
 class OverviewSection extends StatelessWidget {
-  const OverviewSection({super.key});
-
+  const OverviewSection({super.key, required this.jobs});
+  final List<JobModel> jobs;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -16,18 +18,28 @@ class OverviewSection extends StatelessWidget {
       children: [
         Text(
           AppStrings.overview,
-          style: AppStyles.textStyleBold22,
+          style: AppStyles.textStyleBold20,
         ),
         IntrinsicHeight(
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(child: TotalApplicationsCardWidget()),
+              Expanded(
+                child: TotalApplicationsCardWidget(jobs: jobs),
+              ),
               12.horizontalSpace,
-              Expanded(child: ApplicationStatusCardWidget()),
+              Expanded(
+                child: ApplicationStatusCardWidget(
+                  jobs: jobs,
+                ),
+              ),
             ],
           ),
-        )
+        ),
+        20.verticalSpace,
+        ApplicationTypeCardWidget(
+          jobs: jobs,
+        ),
       ],
     );
   }
