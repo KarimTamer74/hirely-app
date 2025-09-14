1536811ed360a257c7487f35536678bfd6fe87a1
KarimTamer74 <kemotamer448@gmail.com>
2025-09-14 14:15:20 +0300

    show application status and link it with backend

--- lib/features/dashboard/presentation/widgets/application_status_card_widget.dart
+++ lib/features/dashboard/presentation/widgets/application_status_card_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hirely/core/common_widgets.dart/custom_card_widget.dart';
+import 'package:hirely/core/helper/enums.dart';
+import 'package:hirely/core/network/job_model.dart';
 import 'package:hirely/core/utils/app_colors.dart';
 import 'package:hirely/core/utils/app_strings.dart';
 import 'package:hirely/core/utils/app_styles.dart';
@@ -11,51 +14,79 @@ import 'package:hirely/features/dashboard/data/models/application_status_model.d
 class ApplicationStatusCardWidget extends StatelessWidget {
   const ApplicationStatusCardWidget({
     super.key,
+    required this.jobs,
   });
-  static List<ApplicationStatusModel> applicationStatusModel = [
-    ApplicationStatusModel(
-        color: AppColors.green, status: 'Accepted', count: '3'),
-    ApplicationStatusModel(
-        color: AppColors.yellow, status: 'Pending', count: '12'),
-    ApplicationStatusModel(
-        color: AppColors.red, status: 'Rejected', count: '6'),
-  ];
+
+  final List<JobModel> jobs;
+
+  
+  List<ApplicationStatusModel> getApplicationStatusCounts() {
+    int acceptedCount = jobs
+        .where((job) => job.applicationStatus == ApplicationStatus.accepted)
+        .length;
+    int pendingCount = jobs
+        .where((job) => job.applicationStatus == ApplicationStatus.pending)
+        .length;
+    int rejectedCount = jobs
+        .where((job) => job.applicationStatus == ApplicationStatus.rejected)
+        .length;
+
+    return [
+      ApplicationStatusModel(
+          color: AppColors.green,
+          status: 'Accepted',
+          count: acceptedCount.toString()),
+      ApplicationStatusModel(
+          color: AppColors.yellow,
+          status: 'Pending',
+          count: pendingCount.toString()),
+      ApplicationStatusModel(
+          color: AppColors.red,
+          status: 'Rejected',
+          count: rejectedCount.toString()),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: AppColors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              AppStrings.status,
-              style:
-                  getMediumStyle(fontSize: FontSize.s16, color: Colors.black45),
+    final statusList = getApplicationStatusCounts();
+
+    return CustomCardWidget(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            AppStrings.status,
+            style: getMediumStyle(
+              fontSize: FontSize.s16,
+              color: AppColors.greyShade600,
             ),
-            10.verticalSpace,
-            ...applicationStatusModel.map((statusItem) {
-              return Row(
+          ),
+          10.verticalSpace,
+          ...statusList.map((statusItem) {
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: 4.h),
+              child: Row(
                 children: [
                   Container(
                     width: 15,
                     height: 15,
                     decoration: BoxDecoration(
-                        color: statusItem.color,
-                        borderRadius: BorderRadius.circular(15)),
+                      color: statusItem.color,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
                   10.horizontalSpace,
                   Text(statusItem.status, style: AppStyles.textStyle16),
-                  Spacer(),
+                  const Spacer(),
                   Text(statusItem.count,
                       style: getMediumStyle(fontSize: FontSize.s18)),
                 ],
-              );
-            }),
-          ],
-        ),
+              ),
+            );
+          }),
+        ],
       ),
     );
   }
